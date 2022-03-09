787d2453b949d02e255499874064c483c459becd
Jefferson Ding <tyding2004@gmail.com>
2022-03-09 15:22:18 -0500

    🟢 Initial Commit

---
+++ Shared/BoardSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardSyncApp.swift
+//  Shared
+//
+//  Created by Jefferson Ding on 2022-03-09.
+//
+
+import SwiftUI
+
+@main
+struct BoardSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
